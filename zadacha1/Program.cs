3c0f83fd0462588ada2e41536adf4fb8b0735bcd
uslan <bublikrs5@mail.ru>
2022-12-08 00:21:25 +0300

    first commit

---
+++ zadacha1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number1 > number2)
+    {
+        Console.WriteLine($"Большее число: {number1}");
+        Console.WriteLine($"Меньшее число: {number2}");
+    }
+if(number2 > number1)
+    {
+        Console.WriteLine($"Большее число: {number2}");
+        Console.WriteLine($"Меньшее число: {number1}");
+    }    
+if(number1 == number2)
+    {
+        Console.WriteLine("Числа равны");
+    }    
---
+++ zadacha2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+ 
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+if(numberA > numberB)
+    if(numberA > numberC)
+        {
+            Console.WriteLine($"Максимальное число: {numberA}");
+        }
+if(numberB > numberA)
+    if(numberB > numberC)
+        {
+            Console.WriteLine($"Максимальное число: {numberB}");
+        }
+if(numberC > numberA)
+    if(numberC > numberB)
+        {
+            Console.WriteLine($"Максимальное число: {numberC}");
+        } 
